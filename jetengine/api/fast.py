dc11d3b70606f31adb8856b42fbe892d7662accb
meghakrishnan <meghagkrishnan1987@gmail.com>
2024-07-22 17:08:49 +0200

    Edited fast.py to upload .txt files for prediction

--- jetengine/api/fast.py
+++ jetengine/api/fast.py
@@ -40,36 +40,3 @@ def upload_file(file: UploadFile = File(...)):
             "Data": first_row,
             "RUL": np.round(float(y_pred))
         })
-
-
-'''
-@app.get("/predict")
-def predict(
-        setting1: float = -0.0037,
-        setting2: float = 0.0001,
-        T24_Total_temperature_at_LPC_outlet: float = 643.04,
-        T30_Total_temperature_at_HPC_outlet: float = 1581.11,
-        T50_Total_temperature_at_LPT_outlet: float =  1405.23,
-        P30_Total_pressure_at_HPC_outlet: float = 554.81,
-        Nf_Physical_fan_speed: float = 2388.05,
-        Nc_Physical_core_speed: float = 9045.9,
-        Ps30_Static_pressure_at_HPC_outlet: float = 47.22,
-        phi_Ratio_of_fuel_flow_to_Ps30: float = 522.07,
-        NRf_Corrected_fan_speed: float = 2388.02,
-        NRc_Corrected_core_speed: float = 8129.71,
-        BPR_Bypass_Ratio: float = 8.421,
-        htBleed_Bleed_Enthalpy: float = 392,
-        W31_HPT_coolant_bleed: float = 39.03,
-        W32_LPT_coolant_bleed: float = 23.422,
-        ):
-    X_pred = pd.DataFrame(locals(), index=[0])
-
-    model = app.state.model
-    assert model is not None
-
-    X_processed = process_Xdata(X_pred)
-    y_pred = model.predict(X_processed)
-
-    return dict(RUL=np.round(float(y_pred)))
-
-'''
